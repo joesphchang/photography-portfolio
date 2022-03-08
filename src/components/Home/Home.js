e15f8c8e22063b73f16534026293ac498e0f9352
joesphchang <joesphschang@gmail.com>
2022-03-07 22:18:42 -0500

    Made more changes to home-page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Home.css';
 import Hero from '../../assets/landing-page.jpg';
+import Hero1 from '../../assets/landing-page-1.JPG';
 
 function Home() {
 	return (
 		<div className='home'>
-			<h1>Photographer / Creative Director</h1>
-			<h2>avantogarde<span className='period'>.</span></h2>
-			<img src={Hero} alt='landing-page' className='home-image'/>
+			<h2>
+				avantogarde<span className='period'>.</span>
+			</h2>
+
+			<img src={Hero} alt='landing-page' className='home-image' />
+			<div>
+				<img src={Hero1} className='home-image-1' alt='landing-page' />
+				<h1>Photographer / Creative Director</h1>
+			</div>
 		</div>
 	);
 }
