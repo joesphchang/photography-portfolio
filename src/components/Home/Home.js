af95de3e5611bc4d354b567567edb845b9f18439
joesphchang <joesphschang@gmail.com>
2022-03-10 11:31:02 -0500

    Updated home-page with new layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,18 +2,24 @@ import React from 'react';
 import './Home.css';
 import Hero from '../../assets/landing-page.jpg';
 import Hero1 from '../../assets/landing-page-1.JPG';
+import Hero2 from '../../assets/landing-page-3.jpg';
 
 function Home() {
 	return (
 		<div className='home'>
-			<h2>
-				avantogarde<span className='period'>.</span>
-			</h2>
-
-			<img src={Hero} alt='landing-page' className='home-image' />
-			<div>
-				<img src={Hero1} className='home-image-1' alt='landing-page' />
-				<h1>Photographer / Creative Director</h1>
+			<div className='image-container'>
+				<div className='image-container-2'>
+					<div className='container'>
+						<img src={Hero} alt='landing-page' className='home-image' />
+						<h1>avantogarde</h1>
+					</div>
+					<div className='container-1'>
+						<img src={Hero2} alt='landing-page' className='home-image' />
+					</div>
+				</div>
+				<div className='image-container-1'>
+					<img src={Hero1} className='home-image-1' alt='landing-page' />
+				</div>
 			</div>
 		</div>
 	);
