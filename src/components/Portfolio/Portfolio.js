014e80e2c8d980e600b3c2b6b06aa5c96b8f305b
joesphchang <joesphschang@gmail.com>
2022-03-16 13:32:37 -0400

    Added styling to the portfolio page

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,27 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import './Portfolio.css';
 
 function Portfolio() {
 	const [photos, setPhotos] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/portfolios')
-      .then((res) => res.json())
-      .then((res) => {
-        setPhotos(res);
-      })
-  }, [])
-
+	useEffect(() => {
+		fetch('http://localhost:3000/api/portfolios')
+			.then((res) => res.json())
+			.then((res) => {
+				setPhotos(res);
+			});
+	}, []);
 
 	return (
 		<div className='portfolio'>
 			<h1>Portfolio</h1>
-      <ul>
-      {photos.map((photo) => {
-        return (
-          <img src={photo.image} alt={photo.title} /> 
-        )
-      })}
-      </ul>
+			<ul>
+				{photos.map((photo) => {
+					return (
+						<li>
+							<img src={photo.image} alt={photo.title} />
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
